d12cc3516d708341f058a18be5f9931a5c2e9b76
Steph Northway <stephanie.northway@gmail.com>
2015-04-04 15:57:15 -0400

    skeleton of EncodedState class, will probably change

--- PacMan1/pacmanAgents.py
+++ PacMan1/pacmanAgents.py
@@ -27,8 +27,10 @@ class GoWestPacman(Agent):
 class ReinforcementPacman(Agent):
 	def __init__(self):
 		super(Agent, self).__init__()
-		self.Q = None
-		self.R = None
+		# How should we initialize these? When will we know the layout??
+		self.Q = np.zeros([1, 1])
+		self.R = np.zeros([1, 1])
+		self.gamma = 1
 
 	def getAction(self,state):
 		"""
@@ -38,12 +40,40 @@ class ReinforcementPacman(Agent):
 		returns: the direction PacMan should go (e.g. Directions.NORTH)
 		"""
 		self.getReward(state)
-		return Directions.WEST
+		return Directions.STOP
 
-	def getReward(self,state):
-		print state.getPacmanPosition()
-		print state.hasFood(*state.getPacmanPosition())
+	def getReward(self, state):
+		# print state.getWalls().asList()
+		print state.data.layout.width, state.data.layout.height
+		# print state.getPacmanPosition()
+		# print state.hasFood(*state.getDirection())
 
-	def updateQ(self, gamma=0.8):
+	def updateQ(self, state, newState):
+		"""
+		Update the Q matrix (brain) of our PacMan with
+		Q(currentState, newState) = R(currentState, newState) + gamma * max(Q(newState, legalActions))
+
+		state: the GameState object
+		newState: 
+		"""
+		self.Q
+
+
+class EncodedState(object):
+	"""
+	We might want this to be its own class?
+	"""
+	def __init__(self):
 		pass
+
+"""
+How will we encode the states? Example: Ghost distance could be stored as the distance to the closest ghost,
+either as the crow flies (rounded) or number of moves it would need to get you.
+Maybe the number of states is not known in our case, and we need to add to Q? But R needs to know all of the states, soo....
+Instead of being directions, what if the actions were based on velocity/current direction vectors...
+- away from closest ghost
+- toward closest food
+- away from where it's already been
+
+"""
 
