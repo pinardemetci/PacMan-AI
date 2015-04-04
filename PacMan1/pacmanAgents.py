6fdd9d570b64670894bcc61b4e1ede2ff5cd04c7
Steph Northway <stephanie.northway@gmail.com>
2015-04-04 14:00:37 -0400

    made Agent inherit from object so we can use super, added R and Q as attributes to our agent

--- PacMan1/pacmanAgents.py
+++ PacMan1/pacmanAgents.py
@@ -3,6 +3,7 @@ Glue for interfacing with the Berkeley code.
 @author Stephanie Northway
 """
 
+import numpy as np
 from game import Agent, Directions
 
 class GoWestPacman(Agent):
@@ -24,10 +25,25 @@ class GoWestPacman(Agent):
 			return Directions.STOP
 
 class ReinforcementPacman(Agent):
+	def __init__(self):
+		super(Agent, self).__init__()
+		self.Q = None
+		self.R = None
+
 	def getAction(self,state):
+		"""
+		Any Agent subclass must define a getAction method.
+
+		state: the GameState object
+		returns: the direction PacMan should go (e.g. Directions.NORTH)
+		"""
 		self.getReward(state)
 		return Directions.WEST
+
 	def getReward(self,state):
 		print state.getPacmanPosition()
 		print state.hasFood(*state.getPacmanPosition())
+
+	def updateQ(self, gamma=0.8):
+		pass
 
