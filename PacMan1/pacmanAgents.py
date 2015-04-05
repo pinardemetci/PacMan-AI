74e156078f89037c948c38b489d100229b1e1745
Steph Northway <stephanie.northway@gmail.com>
2015-04-05 16:57:03 -0400

    Simple exploration agent - renamed ReinforcementPacman to SimpleExplorationPacman - implemented sort-of-q-learning thing where r and q are vectors instead of matrices - added helper functions to break up code into chunks

--- PacMan1/pacmanAgents.py
+++ PacMan1/pacmanAgents.py
@@ -3,8 +3,13 @@ Glue for interfacing with the Berkeley code.
 @author Stephanie Northway
 """
 
+import operator
+import random
+
 import numpy as np
+
 from game import Agent, Directions
+from layout import Layout
 
 class GoWestPacman(Agent):
 	"""
@@ -18,66 +23,174 @@ class GoWestPacman(Agent):
 		This will probably call our actual ML stuff.
 		"""
 		legal_actions = state.getLegalActions()
-		print legal_actions
 		if 'West' in legal_actions:
 			return Directions.WEST
 		else:
 			return Directions.STOP
 
-class ReinforcementPacman(Agent):
+class SimpleExplorationPacman(Agent):
+	"""
+	A Pacman agent that uses a VERY simple q-learning algorithm in which the states
+	have been encoded as "explored" and "unexplored", and Pacman can only be in one
+	state (since he can't be standing in an unexplored cell). That might make it not
+	actually qualify as q-learning, but you know, baby steps.
+	"""
 	def __init__(self):
+<<<<<<< HEAD
+		super(Agent, self).__init__()  # call the parent's init function
+		self.Q = [0, 0]  # Q always initializes with zeros
+		self.R = [1, 5]  # hard-coding R for now
+		self.gamma = 0.3  # exploration parameter
+		self.exploredCoords = set()  # set of coordinates Pacman has been to
+
+	def getAction(self, state):
+=======
 		super(Agent, self).__init__()
 		# How should we initialize these? When will we know the layout??
 		self.Q = None
 		self.R = None
 		self.gamma = 1
-
 	def getAction(self,state):
+>>>>>>> 314770c2d3ca728e7e90661688b904c36cdb8cb3
 		"""
 		Any Agent subclass must define a getAction method.
 
 		state: the GameState object
 		returns: the direction PacMan should go (e.g. Directions.NORTH)
 		"""
-		self.getReward(state)
-		return Directions.STOP
 
+<<<<<<< HEAD
+		# add current position to exploredCoords
+		self.exploredCoords.add(state.getPacmanPosition())
+
+		rewards = self.getActionRewards(state, state.getLegalActions())
+=======
 	def initializeMatrices(self):
-		states = self.data.layout.
+		#states = self.data.layout.
 		self.Q = np.zeros([widthheight])
+>>>>>>> 314770c2d3ca728e7e90661688b904c36cdb8cb3
 
-	def getReward(self, state):
-		# print state.getWalls().asList()
-		print state.data.layout.width, state.data.layout.height
-		# print state.getPacmanPosition()
-		# print state.hasFood(*state.getDirection())
+		if self.goRandomDirection():
+			direction = random.choice(state.getLegalActions())
+			directionReward = rewards[direction]
+		else:
+			direction, directionReward = max(rewards.iteritems(), key=operator.itemgetter(1))  # this currently biases toward last option, probably East
+		
+		self.updateQ(state, self.Q.index(directionReward), set(rewards.values()))
+		
+		return direction
 
-	def updateQ(self, state, newState):
+	def getReward(self, state, action):
+		"""
+		Checks the Q matrix to see what we've got
+		"""
+		if state.generatePacmanSuccessor(action).getPacmanPosition() in self.exploredCoords:
+			return self.Q[0]
+		else:
+			return self.Q[1]
+
+	def getActionRewards(self, state, legalActions):
+		"""
+		Return a dictionary like {direction: reward} for all legal directions
+		"""
+		reward = dict.fromkeys(legalActions)
+		for action in legalActions:
+			reward[action] = self.getReward(state, action)
+		return reward
+
+	def updateQ(self, state, newState, legalActions):
 		"""
 		Update the Q matrix (brain) of our PacMan with
 		Q(currentState, newState) = R(currentState, newState) + gamma * max(Q(newState, legalActions))
 
 		state: the GameState object
-		newState: 
+		newState: 0 or 1 (explored or unexplored) -- sorry this is confusing
+		legalActions: [0] or [0, 1] depending on if any unexplored cells are legally moveable-to
 		"""
+<<<<<<< HEAD
+		self.Q[newState] = self.R[newState] + self.gamma * max(filter(lambda x: x in legalActions, self.Q))
+		print self.Q
+
+=======
 		self.Q
 
+					
+class Node(object):
+	def __init__(self, position):
+		self.position = position
+	def isExplored(self):
+		pass
+
+class Edge(object):
+	def __init__(self, (Node1,Node2)):
+		self.Nodes=(Node1,Node2)
+	def isExplored(self):
+		pass
 
 class EncodedState(object):
+
 	"""
 	We might want this to be its own class?
 	"""
 	def __init__(self, layout):
-		for l in layout.asList()
+		for l in layout.getList():
+			if layout.isWall(l):
+				pass
+			else:
+				x,y = l
+				adjCoordinates=[]
+				adjCoordinates.append(layout.isWall((x+1,y)))
+				adjCoordinates.append(layout.isWall((x-1,y)))
+				adjCoordinates.append(layout.isWall((x,y+1)))
+				adjCoordinates.append(layout.isWall((x,y-1)))
+				count= adjCoordinates.count(True)
+				if count<=1:
+					enc_state[Node((x,y))]=[]
 
-"""
-How will we encode the states? Example: Ghost distance could be stored as the distance to the closest ghost,
-either as the crow flies (rounded) or number of moves it would need to get you.
-Maybe the number of states is not known in our case, and we need to add to Q? But R needs to know all of the states, soo....
-Instead of being directions, what if the actions were based on velocity/current direction vectors...
-- away from closest ghost
-- toward closest food
-- away from where it's already been
+		nodePositions=[]
 
-"""
+		for node in enc_state:
+			pos=node.position
+			nodePositions.append(pos)
+			#positive x axis
+			for i in range(100):
+				if (x+i,y) in nodePositions:
+					a=Edge((node, Node((x+i,y))))
+					enc_state[node].append(a)
+					break
+				else: pass
+			#negative x axis
+			for i in range(100):
+				if (x-i,y) in nodePositions:
+					a=Edge((node, Node((x+i,y))))
+					enc_state[node].append(a)
+					break
+				else: pass
+			#positive y axis
+			for i in range(100):
+				if (x,y+i) in nodePositions:
+					a=Edge((node, Node((x,y+i))))
+					enc_state[node].append(a)
+					break
+				else: pass
+			#negative y axis
+			for i in range(100):
+				if (x,y-i) in nodePositions:
+					a=Edge((node, Node((x,y-i))))
+					enc_state[node].append(a)
+					break
+				else: pass
 
+>>>>>>> 314770c2d3ca728e7e90661688b904c36cdb8cb3
+
+	def goRandomDirection(self):
+		"""
+		Should it go with the highest Q value or try something new?
+
+		returns: boolean
+		"""
+		r = random.random()
+		if r > self.gamma:
+			return True
+		else:
+			return False
