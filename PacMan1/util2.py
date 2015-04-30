b28b70fb0e26fe0b515b3cea4eae62ff77496775
pdemetci <pinardemetci@gmail.com>
2015-04-30 15:03:46 -0400

    This closes #6

--- PacMan1/util2.py
+++ PacMan1/util2.py
@@ -6,6 +6,7 @@ Our own util function to implement the features with Astar instead of MahattanDi
 
 from game import *
 from pacman import *
+import layout
 from numpy import ones
 import copy
 
@@ -17,8 +18,9 @@ def Astar(state, start, goal, layout):
 	costs = {} #The size of the game board to store g, h and f cost values
 	# print layout.width
 	# print layout.height
-	for x in range(layout.width+1):
-		for y in range(layout.height+1):
+	for x in range(layout.width):
+		#print x
+		for y in range(layout.height):
 			costs[(x, y)] = Tile((x,y))
 	# print layout.width
 	# print layout.height
@@ -34,8 +36,8 @@ def Astar(state, start, goal, layout):
 		tile = get_lowest_cost_open_coord(open_list)
 		open_list.remove(tile)
 		closed_list.append(tile)
-		open_coords, relative_direction, tile_cost = get_open_adj_coords(state, tile.coordinates)
-		# print open_coords, relative_direction, tile_cost
+		open_coords, tile_cost = get_open_adj_coords(state, tile.coordinates, layout)
+		# print open_coords, tile_cost
 		for i, coord in enumerate(open_coords):
 			if coord == goal:
 				costs[coord].g_cost = tile.g_cost + tile_cost[i]
@@ -43,7 +45,7 @@ def Astar(state, start, goal, layout):
 				costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
 				# print costs[coord].g_cost
 				# print costs[coord].h_cost
-				# print costs[coord].f_cost
+				print costs[coord].f_cost
 				return costs[coord].f_cost
 				open_list = []
 				break
@@ -60,42 +62,38 @@ def Astar(state, start, goal, layout):
 				if costs[coord].f_cost > costs[coord].g_cost + costs[coord].h_cost:
 					costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
 					costs[coord].parent = tile
-	print open_list
+	#print open_list
 def get_h_cost(coord_a, coord_b):
 	"""Returns the h score, the manhattan distance between coord_a and the cood_b"""
 	return abs(coord_a[0] - coord_b[0]) + abs(coord_a[1] - coord_b[1])
 
 
-def get_open_adj_coords(state, coords):
+def get_open_adj_coords(state, coords, layout):
 	"""returns list of valid coords that are adj. to the pacman, open 
 	(and are not in the closed list(?))"""
 	adj_coords = []
-	d = []
-	configuration_copy = copy.deepcopy(state.getPacmanState().configuration)
-	configuration_copy.pos = coords
+	# configuration_copy = copy.deepcopy(state.getPacmanState().configuration)
+	# configuration_copy.pos = coords
 	# print dir(state)s
 	#print state.__class__.__name__
 	#directions = state.getLegalActions_Tile(configuration_copy, state)
+	walls = layout.isWall(coords)
 	directions = state.getLegalActions()
-	directions.remove("Stop")
-	if "West" in directions:
-		W_coords = (coords[0]-1, coords[1])
-		d.append("West")
-		adj_coords.append(W_coords)
-	if "East" in directions:
-		E_coords = (coords[0]+1, coords[1])
-		d.append("East")
-		adj_coords.append(E_coords)
-	if "North" in directions:
-		N_coords = (coords[0], coords[1]+1)
-		adj_coords.append(N_coords)
-		d.append("North")
-	if "South" in directions:
-		S_coords = (coords[0], coords[1]-1)
-		d.append("South")
-		adj_coords.append(S_coords)
+	#print directions
+	# directions.remove("Stop")
+	if not layout.isWall((coords[0]-1, coords[1])):
+		adj_coords.append((coords[0]-1, coords[1]))
+	if not layout.isWall((coords[0]+1, coords[1])):
+		adj_coords.append((coords[0]+1, coords[1]))
+	if not layout.isWall((coords[0], coords[1]+1)):
+		adj_coords.append((coords[0], coords[1]+1))
+	if not layout.isWall((coords[0], coords[1]-1)):
+		adj_coords.append((coords[0], coords[1]-1))
+	else: pass
+	#print "current coord:", coords
+	#print adj_coords
 	costs = [1]*len(adj_coords)
-	return adj_coords, d, costs
+	return adj_coords, costs
 	
 def get_lowest_cost_open_coord(open_list):
 	"""Return the tile with the lowest cost"""
