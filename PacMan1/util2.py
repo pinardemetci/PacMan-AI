f2e7abc875d15fc1c0f634e3bd22d488b9a17e17
pdemetci <pinardemetci@gmail.com>
2015-05-02 17:37:42 -0400

    Changes to A-star for refining and attempting to fix the NoneType bug

--- PacMan1/util2.py
+++ PacMan1/util2.py
@@ -9,12 +9,11 @@ from pacman import *
 import layout
 from numpy import ones
 import copy
+import util
 
 def Astar(state, start, goal, layout):
 	"""Astar function that runs the show"""
 	open_list = [] #Set of nodes already evaluated
-	closed_list = [] #Tentative nodes to be evaluated
-	current_tile = []
 	costs = {} #The size of the game board to store g, h and f cost values
 	# print layout.width
 	# print layout.height
@@ -30,13 +29,14 @@ def Astar(state, start, goal, layout):
 	costs[start].h_cost = get_h_cost(start, goal)
 	costs[start].f_cost = costs[start].g_cost + costs[start].h_cost
 	open_list.append(costs[start])
-	print "open_list", open_list
+	# print "open_list", open_list
 
 
 	while len(open_list) > 0:
+		# print open_list
 		tile = get_lowest_cost_open_coord(open_list)
 		open_list.remove(tile)
-		closed_list.append(tile)
+		# closed_list.append(tile)
 		open_coords, tile_cost = get_open_adj_coords(state, tile.coordinates, layout)
 		# print open_coords, tile_cost
 		for i, coord in enumerate(open_coords):
@@ -44,26 +44,24 @@ def Astar(state, start, goal, layout):
 				costs[coord].g_cost = tile.g_cost + tile_cost[i]
 				costs[coord].h_cost = get_h_cost(coord, goal)
 				costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
+			# elif layout.isWall(coord) == True:
+			# 	costs[coord].f_cost = 300.0
 				# print costs[coord].g_cost
 				# print costs[coord].h_cost
 
-				print costs[coord], costs[coord].f_cost
+				# print "Coord, cost", costs[coord], costs[coord].f_cost
 				return costs[coord].f_cost
-				open_list = []
-				break
 
-			if costs[coord] in closed_list:
-				pass
 			elif costs[coord] not in open_list:
 				open_list.append(costs[coord])
 				costs[coord].g_cost = tile.g_cost + tile_cost[i]
 				costs[coord].h_cost = get_h_cost(coord, goal)
 				costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
-				costs[coord].parent = tile
-			elif coord in open_list:
+				# return 300
+			else:
 				if costs[coord].f_cost > costs[coord].g_cost + costs[coord].h_cost:
 					costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
-					costs[coord].parent = tile
+				return manhattanDistance((0, 0), (state.data.layout.width, state.data.layout.height))
 	#print open_list
 def get_h_cost(coord_a, coord_b):
 	"""Returns the h score, the manhattan distance between coord_a and the cood_b"""
@@ -105,9 +103,8 @@ class Tile():
 	"""
 	For example layout, food is in [(18, 1), (1,9)]. Bottom left-hand corner = (1,1)
 	"""
-	def __init__(self, coordinates, parent = None, g_cost = None, h_cost = None, f_cost = None):
+	def __init__(self, coordinates, g_cost = None, h_cost = None, f_cost = None):
 		self.coordinates = coordinates
-		self.parent = parent
 		self.g_cost = g_cost
 		self.h_cost = h_cost
 		self.f_cost = f_cost
