c57afdefe68fc30cef75f12f3329e4843c752287
Steph Northway <stephanie.northway@gmail.com>
2015-05-04 11:22:59 -0400

    started commenting util2

--- PacMan1/util2.py
+++ PacMan1/util2.py
@@ -1,52 +1,91 @@
 """
-Pacman Astar
-
-Our own util function to implement the features with Astar instead of MahattanDistance
+@author: kbrennan
+Our own utility functions and classes.
 """
 
 
 from util import manhattanDistance
 
 
+class Tile(object):
+    """
+    Represents a position on the board in terms of what it will cost Pacman to get there.
+    For example layout, food is in [(18, 1), (1,9)]. Bottom left-hand corner = (1,1).
+
+    coordinates: the coordinates of this tile
+    g_cost: total cost of moving the agent from a start position to a given square
+    h_cost: number of total moves (as the crow flies) to goal
+    f_cost: the score for this Tile
+    """
+    def __init__(self, coordinates, g_cost=None, h_cost=None, f_cost=None):
+        self.coordinates = coordinates
+        self.g_cost = g_cost
+        self.h_cost = h_cost
+        self.f_cost = f_cost
+
+    def __str__(self):
+        return str(self.coordinates)
+
+    def __repr__(self):
+        return str(self.coordinates)
+
+
 def initializeTiles(layout):
-    costs = {} #Dictionary the size of the game board to g, h, and f cost values
+    """
+    Initializes the open positions on the board as Tile objects. This
+    gets called once at the beginning of a game.
+
+    layout: the Layout object for the game
+    returns: a dictionary like {(x, y): Tile((x, y))}
+    """
+    costs = {}  # Dictionary the size of the game board to g, h, and f cost values
     for x in range(layout.width):
         for y in range(layout.height):
             costs[(x, y)] = Tile((x, y))
     print "I initialized the tiles"
     return costs
 
-def DepthFirstSearch(state, start):
+
+def DepthFirstSearch(state, start, costs, closed_list=[]):
     """
-    DepthFirstSearch to find close food
+    Depth-first search algorithm to find close food pellets faster than running
+    Astar on every position with food.
+
+    state: the GameState object
+    start: (x, y) coordinates from which to start the search
+    costs: {(x, y): Tile((x, y))}
+    closed_list: list of coordinates no longer being considered
+    returns: Astar distance to the closest food pellet
     """
-    open_list = []
-    open_list.append(start)
-    pass
+    closed_list.append(start)  # start position is off limits
+    # find open adjacent coordinates
+    open_coords = get_open_adj_coords_DFS(state, start, state.data.layout)
+
+    for coord in open_coords:
+        # if there is food at this coordinate, return the distance from Pacman
+        if state.hasFood(*coord):
+            astar_distance = Astar(state, start, coord, state.data.layout, costs)
+            print "Astar value DFS", astar_distance
+            return astar_distance
+        # otherwise, recurse
+        elif coord not in closed_list:
+            DepthFirstSearch(state, coord, costs, closed_list)
+        # othe
+        else:
+            pass
 
 
 def Astar(state, start, goal, layout, costs):
     """Astar function that runs the show"""
     open_list = []  # Set of nodes already evaluated
-    # closed_list = []  # Tentative nodes to be evaluated
-    # costs = {}  # The size of the game board to store g, h and f cost values
-    # print layout.width
-    # print layout.height
-    # print layout.width
-    # print layout.height
-    #print costs
-    #print state.getPacmanState()
     costs[start].g_cost = 0
     costs[start].h_cost = get_h_cost(start, goal)
     costs[start].f_cost = costs[start].g_cost + costs[start].h_cost
     open_list.append(costs[start])
-    # print "open_list", open_list
 
     while len(open_list) > 0:
-        # print open_list
         tile = get_lowest_cost_open_coord(open_list)
         open_list.remove(tile)
-        # closed_list.append(tile)
         open_coords, tile_cost = get_open_adj_coords(state, tile.coordinates, layout)
         # print open_coords, tile_cost
         for i, coord in enumerate(open_coords):
@@ -54,12 +93,7 @@ def Astar(state, start, goal, layout, costs):
                 costs[coord].g_cost = tile.g_cost + tile_cost[i]
                 costs[coord].h_cost = get_h_cost(coord, goal)
                 costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
-            # elif layout.isWall(coord) == True:
-            #     costs[coord].f_cost = 300.0
-                # print costs[coord].g_cost
-                # print costs[coord].h_cost
 
-                # print "Coord, cost", costs[coord], costs[coord].f_cost
                 return costs[coord].f_cost
 
             elif costs[coord] not in open_list:
@@ -72,7 +106,7 @@ def Astar(state, start, goal, layout, costs):
                 if costs[coord].f_cost > costs[coord].g_cost + costs[coord].h_cost:
                     costs[coord].f_cost = costs[coord].g_cost + costs[coord].h_cost
                 return manhattanDistance((0, 0), (state.data.layout.width, state.data.layout.height))
-    #print open_list
+
 
 def get_h_cost(coord_a, coord_b):
     """Returns the h score, the manhattan distance between coord_a and the cood_b"""
@@ -80,52 +114,62 @@ def get_h_cost(coord_a, coord_b):
 
 
 def get_open_adj_coords(state, coords, layout):
-    """returns list of valid coords that are adj. to the pacman, open 
-    (and are not in the closed list(?))"""
+    """
+    Finds the coordinates of the up to four open adjacent positions to coords.
+    coords: the center coordinate pair
+    layout: the Layout object for the game
+    returns: (list of open adjacent coordinates, ones(same length))
+    """
     adj_coords = []
-    # configuration_copy = copy.deepcopy(state.getPacmanState().configuration)
-    # configuration_copy.pos = coords
-    # print dir(state)s
-    #print state.__class__.__name__
-    #directions = state.getLegalActions_Tile(configuration_copy, state)
-    walls = layout.isWall(coords)
-    directions = state.getLegalActions()
-    #print directions
-    # directions.remove("Stop")
-    if not layout.isWall((coords[0]-1, coords[1])):
-        adj_coords.append((coords[0]-1, coords[1]))
-    if not layout.isWall((coords[0]+1, coords[1])):
-        adj_coords.append((coords[0]+1, coords[1]))
-    if not layout.isWall((coords[0], coords[1]+1)):
-        adj_coords.append((coords[0], coords[1]+1))
-    if not layout.isWall((coords[0], coords[1]-1)):
-        adj_coords.append((coords[0], coords[1]-1))
-    else: pass
-    #print "current coord:", coords
-    #print adj_coords
-    costs = [1]*len(adj_coords)
+    if not layout.isWall((coords[0] - 1, coords[1])):
+        adj_coords.append((coords[0] - 1, coords[1]))
+    if not layout.isWall((coords[0] + 1, coords[1])):
+        adj_coords.append((coords[0] + 1, coords[1]))
+    if not layout.isWall((coords[0], coords[1] + 1)):
+        adj_coords.append((coords[0], coords[1] + 1))
+    if not layout.isWall((coords[0], coords[1] - 1)):
+        adj_coords.append((coords[0], coords[1] - 1))
+    else:
+        pass
+
+    costs = [1] * len(adj_coords)
     return adj_coords, costs
 
 
+def get_open_adj_coords_DFS(state, coords, layout):
+    """
+    Get open adjacent coordinates for the DepthFirstSearch function.
+    coords: the center coordinate pair
+    layout: the Layout object for the game
+    returns: list of open adjacent coordinate pairs
+    """
+    adj_coords = []
+    if not layout.isWall((coords[0] - 1, coords[1])):
+        adj_coords.append((coords[0] - 1, coords[1]))
+    if not layout.isWall((coords[0] + 1, coords[1])):
+        adj_coords.append((coords[0] + 1, coords[1]))
+    if not layout.isWall((coords[0], coords[1] + 1)):
+        adj_coords.append((coords[0], coords[1] + 1))
+    if not layout.isWall((coords[0], coords[1] - 1)):
+        adj_coords.append((coords[0], coords[1] - 1))
+    else:
+        pass
+    return adj_coords
+
+
 def get_lowest_cost_open_coord(open_list):
-    """Return the tile with the lowest cost"""
+    """
+    Find the tile with the lowest cost.
+    open_list: list of Tile objects to sort
+    returns: open_list sorted by f_cost
+    """
     return sorted(open_list, key = lambda t: t.f_cost)[0]
 
-class Tile():
-    """
-    For example layout, food is in [(18, 1), (1,9)]. Bottom left-hand corner = (1,1)
-    """
-    def __init__(self, coordinates, g_cost = None, h_cost = None, f_cost = None):
-        self.coordinates = coordinates
-        self.g_cost = g_cost
-        self.h_cost = h_cost
-        self.f_cost = f_cost
-
-    def __str__(self):
-        return str(self.coordinates)
-
-    def __repr__(self):
-        return str(self.coordinates)
 
 def maxManhattanDistance(layout):
+    """
+    Find the maximum Manhattan distance for this layout.
+    layout: the Layout object for the game
+    returns: the Manhattan distance between diagonal corners
+    """
     return manhattanDistance((0, 0), (layout.width, layout.height))
